6d90c7ab3110f712519217e4a4b5d7e505126527
bamamoudou <mamoudouba8787@gmail.com>
2021-03-09 18:32:14 +0100

    Refactoring Gps util service

--- src/main/java/com/tripmaster/gpsutilservice/GpsController.java
+++ src/main/java/com/tripmaster/gpsutilservice/GpsController.java
@@ -1,40 +1,32 @@
 package com.tripmaster.gpsutilservice;
 
-import java.util.List;
+import java.util.UUID;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.tripmaster.common.AttractionData;
-import com.tripmaster.common.User;
-import com.tripmaster.common.VisitedLocationData;
+import com.jsoniter.output.JsonStream;
+
+import gpsUtil.GpsUtil;
 
 @RestController
 public class GpsController {
-	private Logger logger = LoggerFactory.getLogger(GpsController.class);
-	@Autowired private GpsService gpsService;
+	@Autowired
+	private GpsUtil gpsUtil;
 
-	@PatchMapping("/trackAllUserLocations")
-	public List<User> trackAllUserLocations(@RequestBody List<User> userList) {
-		logger.debug("trackAllUserLocations with list of size = " + userList.size());
-		return gpsService.trackAllUserLocations(userList);
-	}
-	
-	@GetMapping("/getAllAttractions")
-	public List<AttractionData> getAllAttractions() {
-		logger.debug("getAllAttractions");
-		return gpsService.getAllAttractions();
+	@GetMapping("/getUserLocation")
+	public String getUserLocation(@RequestParam(required = true) String userId) {
+		if (!StringUtils.isEmpty(userId)) {
+			return JsonStream.serialize(gpsUtil.getUserLocation(UUID.fromString(userId)));
+		}
+		return null;
 	}
 
-	@GetMapping("/getCurrentUserLocation")
-	public VisitedLocationData getCurrentUserLocation(@RequestParam String userId) {
-		logger.debug("getCurrentUserLocation for User " + userId);
-		return gpsService.getCurrentUserLocation(userId);
+	@GetMapping("/getAttractions")
+	public String getAttractions() {
+		return JsonStream.serialize(gpsUtil.getAttractions());
 	}
 }
